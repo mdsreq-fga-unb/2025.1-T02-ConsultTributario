63ab568e859c11be5b17bcd7d41b94cba3296d5c
arturhk05 <arturhandow@gmail.com>
2025-05-10 11:35:34 -0300

    refactor: migra do PostgreSQL para o MongoDB e restrutura o modulo User

--- db/data-source.ts
+++ db/data-source.ts
@@ -2,14 +2,10 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 require('dotenv').config();
 
 export const dataSourceOptions: DataSourceOptions = {
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT as unknown as number,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    type: 'mongodb',
+    url: process.env.MONGO_URL,
     database: process.env.DB_DATABASE,
     entities: ['dist/**/*.entity.js'],
-    migrations: ['dist/db/migrations/*.js'],
 }
 
 const dataSource = new DataSource(dataSourceOptions);
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/data-source';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(dataSourceOptions),
-    UserModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
