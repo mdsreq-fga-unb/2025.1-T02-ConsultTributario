bce0f38e32768a79e8be5c566d920bd7f6eb45b6
Mach1r0 <segundacontafd@gmail.com>
2025-05-13 22:58:43 -0300

    feat: Making the navbar, footer and home page

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <footer className='bg-gray-800 text-white py-4'>
+        <div className='container mx-auto text-center'>
+          <p className='text-sm'>© 2023 Consult Tributário. Todos os direitos reservados.</p>
+          <p className='text-sm'>Desenvolvido por Mash1r0 Academy</p>
+        </div>
+      </footer>
+    </div>
+  )
+}
---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <div className='w-full bg-white shadow-md border-b border-black-200'>
+      <nav className='container mx-auto px-4 py-4 flex justify-between items-center'>
+        <div className='flex items-center'>
+          <Link href={'/'}>
+          <h1 className='text-xl font-bold text-black-600'>Consult Tributário</h1>
+          </Link>
+        </div>
+
+        <div className='hidden md:flex space-x-8 text-black' >
+          <Link href="#funcionalidades" className=' text-black font-medium hover:text-black-600 transition-colors'>
+            Funcionalidades
+          </Link>
+          <Link href="#beneficios" className='text-black font-medium hover:text-black-600 transition-colors'>
+            Benefícios
+          </Link>
+          <Link href="#sobre" className='text-black font-medium hover:text-black-600 transition-colors'>
+            Sobre
+          </Link>
+        </div>
+      
+        <div className='flex space-x-4'>
+          <button className='px-4 py-2 text-blue-600 border border-blue-600 rounded-xl hover:bg-blue-50 transition-colors '>
+            Entrar 
+          </button>
+
+          <button className='px-4 py-2 text-white bg-blue-600 rounded-xl hover:bg-blue-700 transition-colors '>
+            Registrar 
+          </button>
+        </div>
+      </nav>
+    </div>
+  )
+}
