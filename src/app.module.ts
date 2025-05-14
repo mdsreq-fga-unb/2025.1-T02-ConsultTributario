9fa10710207f4025d677f2fc6e9e850b63fa2ff4
Arturhk05 <arturhandow@gmail.com>
2025-05-14 08:50:58 -0300

    chore: add global configuration for environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { QuestionsModule } from './questions/questions.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     MongooseModule.forRoot(
       process.env.MONGO_URL || 'mongodb://localhost:27017/consult-tributario'
     ),
