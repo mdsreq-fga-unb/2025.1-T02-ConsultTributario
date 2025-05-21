a8b6dcf42a1721c33ca4a5b04734fab654c3accf
Diogo Ferreira <pessoaldiogoferreira@gmail.com>
2025-05-21 18:51:54 -0300

    feat(auth): '/login' -> '/auth/login'

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,11 +4,9 @@ import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthRequest } from './models/AuthRequest';
 import { IsPublic } from './decorators/is-public.decorator';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {
-
-    }
+    constructor(private readonly authService: AuthService) { }
 
     @IsPublic()
     @Post('login')
