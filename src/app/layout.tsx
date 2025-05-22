8dac78fb754b3b04e875e47bb5089df3bf572d7d
viniciusvieira00 <vncvieira00@gmail.com>
2025-05-22 11:16:55 -0300

    feat: Ajuste de html body para referenciar-se como pt-BR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={`${inter.className} flex flex-col min-h-screen`}>
         <Navbar />
         <main className="flex-grow">{children}</main>
