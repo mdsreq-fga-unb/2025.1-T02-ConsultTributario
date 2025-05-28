a4c65d9d2930cd879700740fe27ea934315751b1
arturhk05 <arturhandow@gmail.com>
2025-05-27 22:29:31 -0300

    feat(navbar): add AuthNavigation on navbar

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import React from 'react';
 
+import { AuthNavigation } from '@/components/navigation/auth-navigation';
+
 const Navbar = () => {
   return (
     <div className='w-full bg-white shadow-md border-b border-black-200'>
@@ -13,34 +15,26 @@ const Navbar = () => {
 
         <div className='hidden md:flex space-x-8 text-black'>
           <Link
-            href='/funcionalidades/'
+            href='/funcionalidades'
             className='text-lg text-black font-medium hover:text-black-600 transition-colors'
           >
             Funcionalidades
           </Link>
           <Link
-            href='beneficios'
+            href='/beneficios'
             className='text-lg text-black font-medium hover:text-black-600 transition-colors'
           >
             Benefícios
           </Link>
           <Link
-            href='sobre'
+            href='/sobre'
             className='text-lg text-black font-medium hover:text-black-600 transition-colors'
           >
             Sobre
           </Link>
         </div>
 
-        <div className='flex space-x-4'>
-          <button className='px-4 py-2 text-lg text-blue-600 border border-blue-600 rounded-xl hover:bg-blue-50 transition-colors'>
-            Entrar
-          </button>
-
-          <button className='px-4 py-2 text-lg text-white bg-blue-600 rounded-xl hover:bg-blue-700 transition-colors'>
-            Registrar
-          </button>
-        </div>
+        <AuthNavigation />
       </nav>
     </div>
   );
