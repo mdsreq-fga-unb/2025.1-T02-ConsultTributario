a1ff220b1aa372028c1ab61b12fbfdde3e10e7c8
arturhk05 <arturhandow@gmail.com>
2025-06-23 01:23:29 -0300

    feat: add role-based access control to claims controller

--- src/claims/claims.controller.ts
+++ src/claims/claims.controller.ts
@@ -1,10 +1,13 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { ClaimsService } from './claims.service';
 import { Serialize } from '../common/interceptors/serialize.interceptor';
 import { ClaimDto } from './dto/claim.dto';
 import { CreateClaimDto } from './dto/create-claim.dto';
 import { UpdateClaimDto } from './dto/update-claim.dto';
 import { MongoIdValidationPipe } from '@common/pipes/mongo-id-validation.pipe';
+import { Roles } from '@/auth/decorators/roles.decorator';
+import { UserRole } from '@/users/schemas/user.schema';
+import { RolesGuard } from '@/auth/guards/roles.guard';
 
 @Serialize(ClaimDto)
 @Controller('claims')
@@ -12,11 +15,15 @@ export class ClaimsController {
   constructor(private readonly claimsService: ClaimsService) {}
 
   @Post()
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.ADMIN)
   async create(@Body() createClaimDto: CreateClaimDto) {
     return await this.claimsService.create(createClaimDto);
   }
 
   @Get(':id')
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.ADMIN)
   async findById(@Param('id', MongoIdValidationPipe) id: string) {
     return await this.claimsService.findById(id);
   }
@@ -27,6 +34,8 @@ export class ClaimsController {
   }
 
   @Patch(':id')
+  @UseGuards(RolesGuard)
+  @Roles(UserRole.ADMIN)
   async update(@Param('id', MongoIdValidationPipe) id: string, @Body() updateClaimDto: UpdateClaimDto) {
     return await this.claimsService.update(id, updateClaimDto);
   }
