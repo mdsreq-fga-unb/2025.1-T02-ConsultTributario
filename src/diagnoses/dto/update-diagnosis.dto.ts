cab2bcfa6e7833b1519d08856b24e45f81e8b2b3
paulocerqr <paulocerq675@gmail.com>
2025-07-04 16:44:01 -0300

    feat: editar diagnosticos

--- src/diagnoses/dto/update-diagnosis.dto.ts
+++ src/diagnoses/dto/update-diagnosis.dto.ts
@@ -2,3 +2,4 @@ import { PartialType } from '@nestjs/swagger';
 import { CreateDiagnosisDto } from './create-diagnosis.dto';
 
 export class UpdateDiagnosisDto extends PartialType(CreateDiagnosisDto) {}
+//
